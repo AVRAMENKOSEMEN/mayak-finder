c3994ba91ec5d446aa8df3b87561e0d6c5727850
Семён <61841515+AVRAMENKOSEMEN@users.noreply.github.com>
2025-09-23 23:20:53 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,46 @@
-// sw.js - создайте этот файл
-const CACHE_NAME = 'mayak-finder-v1.2';
+// Service Worker для PWA
+const CACHE_NAME = 'mayak-finder-v1.3';
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json'
+  '/',
+  '/index.html',
+  '/style.css', 
+  '/script.js',
+  '/manifest.json',
+  'https://img.icons8.com/fluency/48/000000/bluetooth.png',
+  'https://img.icons8.com/fluency/180/000000/bluetooth.png'
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('Service Worker установлен');
+  console.log('Service Worker: Установлен');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
+        console.log('Service Worker: Кэширование файлов');
         return cache.addAll(urlsToCache);
       })
+      .then(function() {
+        console.log('Service Worker: Все файлы закэшированы');
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker: Активирован');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Удаляем старый кэш', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function() {
+      console.log('Service Worker: Активация завершена');
+      return self.clients.claim();
+    })
   );
 });
 
@@ -23,9 +49,28 @@ self.addEventListener('fetch', function(event) {
     caches.match(event.request)
       .then(function(response) {
         if (response) {
+          console.log('Service Worker: Возвращаем из кэша', event.request.url);
           return response;
         }
-        return fetch(event.request);
+        
+        console.log('Service Worker: Загружаем из сети', event.request.url);
+        return fetch(event.request)
+          .then(function(response) {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          })
+          .catch(function(error) {
+            console.log('Service Worker: Ошибка загрузки', error);
+          });
       })
   );
 });
