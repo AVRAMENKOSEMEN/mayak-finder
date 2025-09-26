5c8968400d60030c689c13600745d05229f7b705
Семён <61841515+AVRAMENKOSEMEN@users.noreply.github.com>
2025-09-26 22:49:13 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
-const CACHE_NAME = 'mayak-finder-v1';
+// Простой Service Worker для PWA
+const CACHE_NAME = 'mayak-finder-map-v1';
 
 self.addEventListener('install', function(event) {
   self.skipWaiting();
+  console.log('Service Worker установлен');
 });
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(self.clients.claim());
+  console.log('Service Worker активирован');
 });
 
 self.addEventListener('fetch', function(event) {
